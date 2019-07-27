53ec68a72a07f372b66793a1b6ea54ac1574a33f
Wil Andrade <andradewilker@gmail.com>
2019-07-27 15:15:31 +0900

    Initial commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+const config = require("./config");
+// const knex = require("knex")(config.db);
+// const models = require("./models")(knex);
+const express = require("express");
+const app = express();
+
+app.listen(config.express.port, () => {
+  console.log("Server running on port ", config.express.port);
+});
