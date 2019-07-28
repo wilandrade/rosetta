57363f07fa6f932e6b823846f4896c9677ae1ccc
Wil Andrade <andradewilker@gmail.com>
2019-07-29 00:25:58 +0900

    Add some more seeds.

--- dev/seed_messages.sql
+++ dev/seed_messages.sql
@@ -1,4 +1,13 @@
 INSERT INTO sentences (username, en_text, jp_text, tag, points)
 VALUES
 ('Wil', 'I am going home at 12 o clock.', '十二時になったら帰ります。', 'Intermediate', 2),
+('Wil', 'Good Morning!', 'おはようございます。', 'Basic', 2),
+('Wil', 'Hello', 'こんにちは', 'Basic', 1),
+('Wil', 'Goodnight', 'おやすみなさい', 'Basic', 3),
+('Wil', 'You are already dead!', 'お前はもう死んでいる', 'Anime', 9001),
+('Wil', 'Thanks for Your Hard Work', 'お疲れ様でした', 'Basic', 0),
+('Wil', 'Good Weather, huh!', 'いい天気ですね!', 'Basic', 4),
+('Wil', 'How are you?', '元気ですか', 'Basic', 2),
+('Wil', 'Long Time, No See!', '久しぶり！', 'Basic', 2),
+('Wil', 'Good Evening', 'こんばんは', 'Basic', 0),
 ('Wil', 'See ya!', 'じゃあね', 'Basic', 0);
